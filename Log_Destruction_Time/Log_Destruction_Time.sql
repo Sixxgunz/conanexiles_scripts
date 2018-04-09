3059e894068dcc77eb9985cb5af54d95732235a2
Sixxgunz <tekkeryole2@gmail.com>
2018-04-09 18:48:25 -0500

    Update Log_Destruction_Time.sql

--- Log_Destruction_Time/Log_Destruction_Time.sql
+++ Log_Destruction_Time/Log_Destruction_Time.sql
@@ -1,4 +1,6 @@
 --This is only a prototype as I'm still testing performance issues and accuracy - Use at your own risk
+
+--Here we alter the destruction history table
 ALTER TABLE destruction_history ADD COLUMN Time_Destroyed timestamp DEFAULT NULL /* replace me */;
 UPDATE destruction_history SET Time_Destroyed = CURRENT_TIMESTAMP;
 PRAGMA writable_schema = on;
@@ -8,17 +10,21 @@ SET sql = replace(sql, 'DEFAULT NULL /* replace me */',
 WHERE type = 'table'
   AND name = 'destruction_history';
 PRAGMA writable_schema = off;
+
+--Now we add the trigger
 DROP TRIGGER IF EXISTS Log_Destruction_Time;
 CREATE TRIGGER Log_Destruction_Time
-AFTER UPDATE
-ON destruction_history
-FOR EACH ROW
-BEGIN
-UPDATE destruction_history SET Time_Destroyed = CURRENT_TIMESTAMP
-WHERE destroyed_by=new.destroyed_by
-and owner_id=old.owner_id
-OR object_type=new.object_type
-and owner_id=old.owner_id
-OR object_id=new.object_id
-and owner_id=old.owner_id;
-END;
+AFTER
+UPDATE
+  ON destruction_history FOR EACH ROW BEGIN
+UPDATE
+  destruction_history
+SET
+  Time_Destroyed = CURRENT_TIMESTAMP
+WHERE
+  destroyed_by = new.destroyed_by
+  and owner_id = old.owner_id
+  and (
+    object_type = new.object_type
+    OR object_id = new.object_id
+  );END
