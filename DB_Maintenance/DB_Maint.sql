71a5d2a8fbabfbb705fc8f1c8bcae8b14bfa3a6d
Sixxgunz <tekkeryole2@gmail.com>
2018-11-01 17:22:02 -0500

    Update DB_Maint.sql

--- DB_Maintenance/DB_Maint.sql
+++ DB_Maintenance/DB_Maint.sql
@@ -1,7 +1,10 @@
+--Use top section at your own risk, the uncommented section is the maintenance section.
+/*
 PRAGMA default_cache_size=700000;
 PRAGMA cache_size=700000;
 PRAGMA PAGE_SIZE = 4096;
 PRAGMA auto_vacuum = FULL;
+*/
 VACUUM;
 REINDEX;
 ANALYZE;
