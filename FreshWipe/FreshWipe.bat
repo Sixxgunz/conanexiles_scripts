03927599bd39d089877e0c0a06430b43e739a42d
Sixxgunz <tekkeryole2@gmail.com>
2018-11-07 02:36:25 -0600

    just cleaning up some text info

--- FreshWipe/FreshWipe.bat
+++ FreshWipe/FreshWipe.bat
@@ -1,9 +1,8 @@
-::Please read clean.sql for what this contains. This is what I use on my server to help clear out inactive players.
-::This is a very lengthy process, but at this time the devs have not come up with a better solution
-::And our current decay script has been deprecated as it no longer removes character and structure data properly.
-::Place this file in a directory with the Imposter_Script.sql.
+::Please read InactiveCleanout.sql for what this contains. This is what I use on my server to help clear out inactive players.
+::This can be a very lengthy process if you have a large database, but at this time the devs have not come up a solution to decay
+::Place this file in a directory with the InactiveCleanout.sql.
 ::Make sure you backup a copy of your main database into a seperate directory in case we run into any issues down the road.
-::Run.
+::And now lets break some stuff.
 @echo off
 for %%a in (game.db) do (
 echo "%%a"
