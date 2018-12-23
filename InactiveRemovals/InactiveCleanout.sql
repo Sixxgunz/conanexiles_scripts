8409c1e2250b0de3430c427ef4dba4260e980a57
Sixxgunz <tekkeryole2@gmail.com>
2018-12-23 11:13:17 -0600

    nvm, totally needs it

--- InactiveRemovals/InactiveCleanout.sql
+++ InactiveRemovals/InactiveCleanout.sql
@@ -42,7 +42,7 @@ delete from item_inventory where owner_id in (select id from characters where la
 delete from item_inventory where owner_id in (select guildid from guilds where guildid not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-5 days') and guild is not null));
 delete from actor_position where id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-5 days') and id not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-5 days') and guild is not null));
 delete from actor_position where id in (select guildid from guilds where guildid not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-5 days') and guild is not null));
-delete from guilds where guildid not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-5 days'));
+delete from guilds where guildid not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-5 days') and guildid not NULL);
 delete from character_stats where char_id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-5 days') and guild is null);
 delete from character_stats where char_id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-5 days') and guild not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-5 days') and guild is not null));
 delete from characters where id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-5 days') and guild is null);
