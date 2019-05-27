d81064ea2d262c535686e4c803ca4e3af84acb0e
Sixxgunz <tekkeryole2@gmail.com>
2019-05-27 09:09:35 -0500

    Update FreshWipeKeepPippiServerData.sql

--- FreshWipe/FreshWipeKeepPippiServerData.sql
+++ FreshWipe/FreshWipeKeepPippiServerData.sql
@@ -1,32 +1,18 @@
 ------------------------------------------------------------------
---  This does not delete anything from the accounts table       --
---  I keep personal records of all steam id's for the purpose   --
---  of tracking total number of players that have joined since  -- 
---  conan exiles original release day when we started WWG.      --
---  This script in particular is set up to keep all pippi info  --
---  If you are not using pippi, have no fear it is still able   --
---  to fresh wipe your database and keep all character/clan info--
---  if you wish, if you wish to delete ALL data then remove the --
---  comments for characters/guilds table.                       --
+--  Fresh Wipe Keep all Pippi Server Data                       --
 ------------------------------------------------------------------
 --Wipe DB keeping only base character/guild in order to keep pippi information intact
---DELETE FROM account;
 DELETE FROM actor_bounding_box;
-DELETE FROM actor_position where class NOT LIKE'%systemconfigs%';--Save Pippi Settings through wipe cycles.;
+DELETE FROM actor_position where class NOT LIKE'%pippi%';--Save Pippi Settings through wipe cycles.;
 DELETE FROM buildable_health;
 DELETE FROM building_instances;
 DELETE FROM buildings;
 DELETE FROM character_stats;
---Keep Character Info through wipe cycles to keep pippi info
---DELETE FROM characters;
 DELETE FROM destruction_history;
 DELETE FROM events;
 DELETE FROM game_events;
---Keep Character Guild Info through wipe cycles to keep pippi info
---DELETE FROM guilds;
 DELETE FROM item_inventory;
---Saving Pippi info through wipe cycles
---DELETE FROM item_properties;
+DELETE FROM item_properties;
 DELETE FROM properties where name NOT LIKE'%pippi%';--Save Pippi Settings through wipe cycles.
 DELETE FROM purgescores;
 DELETE FROM static_buildables;
